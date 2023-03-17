79a3f4c484c08e61d026b487e28718e443b85f81
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-17 16:14:04 +0630

    Update authentication.py

--- bot/plugins/authentication.py
+++ bot/plugins/authentication.py
@@ -38,38 +38,6 @@ async def _auth(c: UtubeBot, m: Message) -> None:
             cred_data = f.read()
 
         log.debug(f"Authentication success, auth data saved to {Config.CRED_FILE}")
-
-        msg2 = await msg.reply_text(cred_data, parse_mode=None)
-        await msg2.reply_text(
-            "This is your authorisation data! Save this for later use. Reply /save_auth_data to the authorisation "
-            "data to re authorise later.",
-            True,
-        )
-
-    except Exception as e:
-        log.error(e, exc_info=True)
-        await m.reply_text(tr.AUTH_FAILED_MSG.format(e), True)
-
-
-@UtubeBot.on_message(
-    Filters.private
-    & Filters.incoming
-    & Filters.command("save_auth_data")
-    & Filters.reply
-    & Filters.user(Config.AUTH_USERS)
-)
-async def _save_auth_data(c: UtubeBot, m: Message) -> None:
-    auth_data = m.reply_to_message.text
-    try:
-        with open(Config.CRED_FILE, "w") as f:
-            f.write(auth_data)
-
-        auth = GoogleAuth(Config.CLIENT_ID, Config.CLIENT_SECRET)
-        auth.LoadCredentialsFile(Config.CRED_FILE)
-        auth.authorize()
-
-        await m.reply_text(tr.AUTH_DATA_SAVE_SUCCESS, True)
-        log.debug(f"Authentication success, auth data saved to {Config.CRED_FILE}")
     except Exception as e:
         log.error(e, exc_info=True)
         await m.reply_text(tr.AUTH_FAILED_MSG.format(e), True)
