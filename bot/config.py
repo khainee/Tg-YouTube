fd838e619b73649da4e2d7497cebe3e3fe139617
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-17 15:37:12 +0630

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,19 +2,19 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "6252189625:AAGruZXEm2BqLYODWldLwHA2IwEFj9-5xqE"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "utube"
 
-    API_ID = ""
+    API_ID = "10112326"
 
-    API_HASH = ""
+    API_HASH = "76b4e277c75aaf991589d78eca42946a"
 
     CLIENT_ID = ""
 
     CLIENT_SECRET = ""
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [5227230295]
 
     VIDEO_DESCRIPTION = ""
 
